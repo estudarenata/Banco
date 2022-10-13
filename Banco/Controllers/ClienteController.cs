06c51b4df579cfc3febcb9b4402f045d16dd40be
estudarenata <renatalobo@prof.educacao.sp.gov.br>
2022-10-13 09:11:19 -0300

    Adicionar arquivos de projeto.

---
+++ Banco/Controllers/ClienteController.cs
@@ -0,0 +1,24 @@
+﻿using Banco.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banco.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class ClienteController : ControllerBase
+    {
+        private static List<Cliente> clientes = new List<Cliente>();
+
+        [HttpPost]
+        public void AdicionaCliente([FromBody]Cliente cliente)
+        {
+            clientes.Add(cliente);
+            Console.WriteLine(cliente.Nome);
+        }
+    }
+}
---
+++ Banco/Data/ClienteContext.cs
@@ -0,0 +1,19 @@
+﻿using Banco.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Banco.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> opt) : base (opt)
+        {
+            
+        }
+
+        public  DbSet<Cliente> Clientes { get; set; }
+    }
+}
